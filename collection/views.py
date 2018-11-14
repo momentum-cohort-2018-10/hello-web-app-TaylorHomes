9d963ed7774e3cd580e72375eff0d6efc1051dbc
TaylorHomes <taylor.homes01@gmail.com>
2018-11-14 15:14:59 -0500

    Template Tags: A Love Story

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+
+def index(request): 
+    number = 6
+# don't forget the quotes because it's a string, not an integer
+    thing = "Thing "
+    return render(request, 'index.html', {
+        'number': number,
+# don't forget to pass it in, and the last comma 'thing': thing,
+})
+
+
+def index(request): 
+    number = 6
+# don't forget the quotes because it's a string, not an integer
+    thing = "Thing name"
+    return render(request, 'index.html', {
+        'number': number,
+# don't forget to pass it in, and the last comma 
+         'thing': thing,
+})
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path
 from collection import views
 
+
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('admin/', admin.site.urls), 
-    ]
+    path('', views.index, name='home'), 
+    path('about/',TemplateView.as_view(template_name='about.html'),name='about'), 
+    path('contact/',TemplateView.as_view(template_name='contact.html'),name='contact'),
+    path('admin/', admin.site.urls), ]
