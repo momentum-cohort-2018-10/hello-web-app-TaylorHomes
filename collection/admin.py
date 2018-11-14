9b47c057b2e39284f7d81c7631ba6a056b70c9b9
TaylorHomes <taylor.homes01@gmail.com>
2018-11-14 17:39:55 -0500

    So many things, now with admin access

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from collection.models import Villians
+
+
+class VillainAdmin(admin.ModelAdmin):
+    model = Villians
+    list_display = (
+        'name',
+        'description',
+    )
+    prepopulated_fields = {'slug': ('name', )}
+
+
+admin.site.register(Villians, VillainAdmin)
+
 # Register your models here.
--- collection/views.py
+++ collection/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 
 
-def index(request): 
-    number = 6
-# don't forget the quotes because it's a string, not an integer
-    thing = "Thing "
-    return render(request, 'index.html', {
-        'number': number,
-# don't forget to pass it in, and the last comma 'thing': thing,
-})
+# def index(request): 
+#     number = 6
+# # don't forget the quotes because it's a string, not an integer
+#     thing = "Thing "
+#     return render(request, 'index.html', {
+#         'number': number,
+# # don't forget to pass it in, and the last comma 'thing': thing,
+# })
 
 
 def index(request): 
     number = 6
 # don't forget the quotes because it's a string, not an integer
-    thing = "Thing name"
+    villain = "Villain name"
     return render(request, 'index.html', {
         'number': number,
 # don't forget to pass it in, and the last comma 
-         'thing': thing,
+         'villain': villain,
 })
