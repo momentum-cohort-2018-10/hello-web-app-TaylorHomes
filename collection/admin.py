d9a33e843736ee75a45cf8b230164457e389cb94
TaylorHomes <taylor.homes01@gmail.com>
2018-11-14 22:22:05 -0500

    Stuck on error within querying for info from database.

--- collection/admin.py
+++ collection/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from collection.models import Villians
 
 
-class VillainAdmin(admin.ModelAdmin):
+class VillainsAdmin(admin.ModelAdmin):
     model = Villians
     list_display = (
         'name',
@@ -12,6 +12,6 @@ class VillainAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
 
-admin.site.register(Villians, VillainAdmin)
+admin.site.register(Villians, VillainsAdmin)
 
 # Register your models here.
--- collection/views.py
+++ collection/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
+from collection.models import Villains
 
 
-# def index(request): 
-#     number = 6
-# # don't forget the quotes because it's a string, not an integer
-#     thing = "Thing "
-#     return render(request, 'index.html', {
-#         'number': number,
-# # don't forget to pass it in, and the last comma 'thing': thing,
-# })
-
-
-def index(request): 
-    number = 6
-# don't forget the quotes because it's a string, not an integer
-    villain = "Villain name"
+def index(request):
+    villains = Villains.objects.all()
     return render(request, 'index.html', {
-        'number': number,
-# don't forget to pass it in, and the last comma 
-         'villain': villain,
-})
+        'villains': villains,
+    })
