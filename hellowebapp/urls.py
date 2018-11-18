e83c099f51e81415a2f841f62ae85b84883d21ea
TaylorHomes <taylor.homes01@gmail.com>
2018-11-17 19:59:10 -0500

    Latest commit, not much progress.

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf.urls import path, include
+from django.urls import path
 from django.contrib import admin
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.views.generic import TemplateView, RedirectView
 
-from collection.backends import MyRegistrationView
 from collection import views
 
 urlpatterns = [
@@ -35,37 +39,28 @@ urlpatterns = [
     path('villain/', RedirectView.as_view(
         pattern_name='browse', permanent=True)),
     path('villain/<slug>/', views.villain_detail, name='villain_detail'),
-    path('villain/<slug>/edit/', views.edit_villan, name='edit_villain'),
+    path('villain/<slug>/edit/', views.edit_villain, name='edit_villain'),
     path('browse/', RedirectView.as_view(
         pattern_name='browse', permanent=True)),
-    path('browse/name/', views.browse_by_name, name='browse'),
-    path(
-        'browse/name/<initial>/', views.browse_by_name, name='browse_by_name'),
     path(
         'accounts/password/reset/',
-        password_reset,
-        {'template_name': 'registration/password_reset_form.html'},
+        PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html'),
         name="password_reset"),
     path(
         'accounts/password/reset/done/',
-        password_reset_done,
-        {'template_name': 'registration/password_reset_done.html'},
+        PasswordResetView.as_view(
+            template_name='registration/password_reset_done.html'),
         name="password_reset_done"),
     path(
         'accounts/password/reset/<uidb64>/<token>/',
-        password_reset_confirm,
-        {'template_name': 'registration/password_reset_confirm.html'},
+        PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'),
         name="password_reset_confirm"),
     path(
         'accounts/password/done/',
-        password_reset_complete,
-        {'template_name': 'registration/password_reset_complete.html'},
+        PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'),
         name="password_reset_complete"),
-
-    # path('accounts/register/',
-    #     MyRegistrationView.as_view(), name='registration_register'),
-    # path('accounts/create_thing/',
-    #     views.create_thing, name='registration_create_thing'),
-    path('accounts/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
 ]
